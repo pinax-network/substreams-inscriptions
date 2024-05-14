ba26cf640ebdcbf7f06ce3bb8a73710322bda64c
YaroShkvorets <shkvorets@gmail.com>
2024-05-14 19:21:31 -0400

    rewrite map_operations in more idiomatic way

--- src/maps.rs
+++ src/maps.rs
@@ -1,92 +1,80 @@
 use crate::helpers::{json_to_i64, json_to_string};
-use crate::pb::inscriptions::types::v1::{DeployOp, TransferOp};
-use crate::pb::inscriptions::types::v1::{MintOp, Operations, OperationEvent, Transactions, operation_event::Operation};
-use substreams::errors::Error;
-use substreams::log;
+use crate::pb::inscriptions::types::v1::{
+    operation_event::Operation, DeployOp, MintOp, OperationEvent, Operations, Transactions,
+    TransferOp,
+};
+use substreams::{errors::Error, log};
 
 #[substreams::handlers::map]
 pub async fn map_operations(transactions: Transactions) -> Result<Operations, Error> {
-    let mut operations = vec![];
-
-    for transaction in transactions.transactions {
-        // Parse JSON data
-        let json_data = match serde_json::from_str(&transaction.data) {
-            Ok(data) => data,
-            Err(_e) => continue,
-        };
-
-        let tick = json_to_string(&json_data, "tick");
-        let op = json_to_string(&json_data, "op");
-        let p = json_to_string(&json_data, "p");
-
-        log::debug!("Data: {}", transaction.data);
-
-        // mint
-        if op == "mint" {
-            let amt = json_to_i64(&json_data, "amt");
-            if amt.is_none() {
-                continue;
-            }
-
-            let operation = MintOp {
-                p: p.clone(),
-                op,
-                tick,
-                amt: amt.unwrap(),
+    let operations = transactions
+        .transactions
+        .into_iter()
+        .filter_map(|transaction| {
+            let json_data = match serde_json::from_str(&transaction.data) {
+                Ok(data) => data,
+                Err(_) => return None,
             };
 
-            operations.push(OperationEvent {
-                transaction: Some(transaction),
-                operation: Some(Operation::Mint(operation)),
-            });
-            continue;
-        }
+            let tick = json_to_string(&json_data, "tick");
+            let op = json_to_string(&json_data, "op");
+            let p = json_to_string(&json_data, "p");
 
-        // transfer
-        if op == "transfer" {
-            let amt = json_to_i64(&json_data, "amt");
-            if amt.is_none() {
-                continue;
+            log::debug!("Data: {}", transaction.data);
+
+            match op.as_str() {
+                "mint" => {
+                    let amt = match json_to_i64(&json_data, "amt") {
+                        Some(amt) => amt,
+                        None => return None,
+                    };
+
+                    let operation = MintOp { p, op, tick, amt };
+
+                    Some(OperationEvent {
+                        transaction: Some(transaction),
+                        operation: Some(Operation::Mint(operation)),
+                    })
+                }
+                "transfer" => {
+                    let amt = match json_to_i64(&json_data, "amt") {
+                        Some(amt) => amt,
+                        None => return None,
+                    };
+                    let operation = TransferOp { p, op, tick, amt };
+
+                    Some(OperationEvent {
+                        transaction: Some(transaction),
+                        operation: Some(Operation::Transfer(operation)),
+                    })
+                }
+                "deploy" => {
+                    let max = match json_to_i64(&json_data, "max") {
+                        Some(max) => max,
+                        None => return None,
+                    };
+                    let lim = match json_to_i64(&json_data, "lim") {
+                        Some(lim) => lim,
+                        None => return None,
+                    };
+                    let operation = DeployOp {
+                        p,
+                        op,
+                        tick,
+                        max,
+                        lim,
+                    };
+
+                    Some(OperationEvent {
+                        transaction: Some(transaction),
+                        operation: Some(Operation::Deploy(operation)),
+                    })
+                }
+                _ => None,
             }
-            let operation = TransferOp {
-                p,
-                op,
-                tick,
-                amt: amt.unwrap(),
-            };
-
-            operations.push(OperationEvent {
-                transaction: Some(transaction),
-                operation: Some(Operation::Transfer(operation)),
-            });
-            continue;
-        }
-
-        // deploy
-        if op == "deploy" {
-            let max = json_to_i64(&json_data, "max");
-            let lim = json_to_i64(&json_data, "lim");
-            if max.is_none() || lim.is_none() {
-                continue;
-            }
-            let operation = DeployOp {
-                p,
-                op,
-                tick,
-                max: max.unwrap(),
-                lim: lim.unwrap(),
-            };
-
-            operations.push(OperationEvent {
-                transaction: Some(transaction),
-                operation: Some(Operation::Deploy(operation)),
-            });
-            continue;
-        }
-    }
+        })
+        .collect::<Vec<_>>();
 
     log::debug!("Operations: {:?}", operations.len());
-    Ok(Operations {
-        operations
-    })
+    Ok(Operations { operations })
 }

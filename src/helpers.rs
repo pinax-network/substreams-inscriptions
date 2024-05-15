071220d833135c60faa5b9be73e5fb9b80436d02
YaroShkvorets <shkvorets@gmail.com>
2024-05-15 00:23:52 -0400

    fix clippy warnings, refactor map

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,90 +3,61 @@ use substreams::Hex;
 use substreams_ethereum::pb::eth::v2::BigInt;
 
 pub fn json_to_i64(value: &serde_json::Value, index: &str) -> Option<i64> {
-    if let Some(value) = value.get(index) {
-        if let Some(str) = value.as_str() {
-            let parsed = str.parse::<i64>();
-            match parsed {
-                Ok(parsed) => Some(parsed),
-                Err(_e) => None,
-            }
-        } else {
-            None
-        }
-    } else {
-        None
-    }
+    value
+        .get(index)
+        .and_then(|v| v.as_str())
+        .and_then(|s| s.parse::<i64>().ok())
 }
 
 pub fn json_to_string(value: &serde_json::Value, index: &str) -> String {
-    match value.get(index) {
-        Some(value) => {
-            match value.as_str() {
-                Some(str) => str.to_string(),
-                None => "".to_string(),
-            }
-        },
-        None => "".to_string(),
-    }
+    value
+        .get(index)
+        .and_then(|v| v.as_str())
+        .map_or(String::new(), |s| s.to_string())
 }
 
-pub fn parse_value(value: &Option<BigInt>) -> String {
-    match value {
-        Some(big_int) => {
-            if Hex(&big_int.bytes).to_string().len() == 0 {
-                String::from("0")
-            } else {
-                Hex(&big_int.bytes).to_string()
-            }
-        },
-        None => String::from("0")
+pub fn parse_value(value: &BigInt) -> String {
+    let encoded = Hex(&value.bytes).to_string();
+    if encoded.is_empty() {
+        String::from("0")
+    } else {
+        encoded
     }
 }
 
 pub fn parse_data(utf8: &str) -> Option<&str> {
-    if utf8.starts_with("data:") == false {
-        return None;
-    }
-    utf8.splitn(2, ',').nth(1)
+    utf8.strip_prefix("data:")
+        .and_then(|s| s.split_once(',').map(|(_, rest)| rest))
 }
 
 pub fn _hex_to_utf8(hex: &str) -> Option<String> {
-    let bytes =  Hex::decode(hex);
-    match bytes {
-        Ok(bytes) => bytes_to_utf8(&bytes),
-        Err(_e) => None,
-    }
+    Hex::decode(hex)
+        .ok()
+        .and_then(|bytes| bytes_to_utf8(&bytes))
 }
 
-pub fn bytes_to_utf8(bytes: &Vec<u8>) -> Option<String> {
-    if bytes.is_empty() {
-        return None;
-    }
-    match str::from_utf8(&bytes) {
-        Ok(vec) => Some(vec.to_string()),
-        Err(_e) => None,
-    }
+pub fn bytes_to_utf8(bytes: &[u8]) -> Option<String> {
+    str::from_utf8(bytes).ok().map(|s| s.to_string())
 }
 
 pub fn parse_mime_type(utf8: &str) -> Option<&str> {
-    // split "data:application/json,inscription" into "data" and "application/json,inscription"
-    let parts = utf8.splitn(2, "data:").nth(1);
+    let parts = utf8.split_once("data:").map(|x| x.1);
     match parts {
         Some(parts) => {
             // split "application/json,inscription" into "application/json" and "inscription"
-            let subparts = parts.splitn(2, ',').nth(0);
-            return match subparts {
+            let subparts = parts.split(',').next();
+            match subparts {
                 Some(subparts) => {
                     // if no subparts "data:,inscription" then return "text/plain"
-                    if subparts.len() == 0 {
-                        return Some("text/plain")
+                    if subparts.is_empty() {
+                        return Some("text/plain");
                     }
                     Some(subparts)
-                },
+                }
                 None => None,
             }
-        },
-        None => return None,
+        }
+        None => None,
     }
 }
 
@@ -106,7 +77,7 @@ mod tests {
             Ok(data) => {
                 assert_eq!(super::json_to_string(&data, "op"), "mint");
                 assert_eq!(super::json_to_string(&data, "tick"), "bull");
-            },
+            }
             Err(e) => {
                 panic!("Error: {}", e);
             }
@@ -123,7 +94,7 @@ mod tests {
         match data {
             Ok(data) => {
                 assert_eq!(super::json_to_i64(&data, "amt").unwrap(), 100);
-            },
+            }
             Err(e) => {
                 panic!("Error: {}", e);
             }
@@ -142,7 +113,7 @@ mod tests {
             Ok(data) => {
                 assert_eq!(super::json_to_i64(&data, "max").is_none(), true);
                 assert_eq!(super::json_to_i64(&data, "lim").unwrap(), 100000000000000);
-            },
+            }
             Err(e) => {
                 panic!("Error: {}", e);
             }
@@ -159,12 +130,17 @@ mod tests {
         match data {
             Ok(data) => {
                 let data_map = data.as_object().expect("Data must be an object");
-                let data_str = data_map.get("value").expect("Expected 'value' field").as_str().expect("Expected 'value' to be a string");
-                let data_hex = Hex::decode(data_str.as_bytes()).expect("Failed to decode hex string");
-                let data_bigint = Some(BigInt { bytes: data_hex });
+                let data_str = data_map
+                    .get("value")
+                    .expect("Expected 'value' field")
+                    .as_str()
+                    .expect("Expected 'value' to be a string");
+                let data_hex =
+                    Hex::decode(data_str.as_bytes()).expect("Failed to decode hex string");
+                let data_bigint = BigInt { bytes: data_hex };
 
                 assert_eq!(super::parse_value(&data_bigint), "646174613a2c7b2270223a226173632d3230222c226f70223a226465706c6f79222c227469636b223a22786169222c226d6178223a22322e31652b3239222c226c696d223a22313030303030303030303030303030227d");
-            },
+            }
             Err(e) => {
                 panic!("Error: {}", e);
             }
@@ -181,10 +157,10 @@ mod tests {
                 let tick = super::json_to_string(&json_data, "tick");
                 let op = super::json_to_string(&json_data, "op");
                 let p = super::json_to_string(&json_data, "p");
-                assert_eq!(tick, "xai");  //tick
+                assert_eq!(tick, "xai"); //tick
                 assert_eq!(op, "deploy"); //op
                 assert_eq!(p, "asc-20"); //p
-            },
+            }
             Err(e) => {
                 panic!("Error: {}", e);
             }
@@ -206,24 +182,39 @@ mod tests {
 
     #[test]
     fn bytes_to_utf8() {
-        assert_eq!(super::bytes_to_utf8(&vec![0x64, 0x61, 0x74, 0x61]).unwrap(), "data");
+        assert_eq!(
+            super::bytes_to_utf8(&vec![0x64, 0x61, 0x74, 0x61]).unwrap(),
+            "data"
+        );
     }
 
     #[test]
     fn parse_mime_type() {
         // valid
-        assert_eq!(super::parse_mime_type(&"data:application/json".to_string()).unwrap(), "application/json");
-        assert_eq!(super::parse_mime_type(&"data:text/plain".to_string()).unwrap(), "text/plain");
-        assert_eq!(super::parse_mime_type(&"data:".to_string()).unwrap(), "text/plain");
-        assert_eq!(super::parse_mime_type(&"data:application/vnd.facet.tx+json;rule=esip6,{\"data\":".to_string()).unwrap(), "application/vnd.facet.tx+json;rule=esip6");
+        assert_eq!(
+            super::parse_mime_type("data:application/json").unwrap(),
+            "application/json"
+        );
+        assert_eq!(
+            super::parse_mime_type("data:text/plain").unwrap(),
+            "text/plain"
+        );
+        assert_eq!(super::parse_mime_type("data:").unwrap(), "text/plain");
+        assert_eq!(
+            super::parse_mime_type("data:,inscription").unwrap(),
+            "text/plain"
+        );
+        assert_eq!(
+            super::parse_mime_type("data:application/vnd.facet.tx+json;rule=esip6,{\"data\":")
+                .unwrap(),
+            "application/vnd.facet.tx+json;rule=esip6"
+        );
 
         // invalid
-        assert_eq!(super::parse_mime_type(&"foobar".to_string()).is_none(), true);
-        assert_eq!(super::parse_mime_type(&"".to_string()).is_none(), true);
+        assert_eq!(super::parse_mime_type("foobar").is_none(), true);
+        assert_eq!(super::parse_mime_type("").is_none(), true);
     }
-
 }
-
 
 // Tests
 // -----
--- src/indexes.rs
+++ src/indexes.rs
@@ -1,63 +1,65 @@
-use crate::helpers::{parse_data, bytes_to_utf8, parse_mime_type, parse_value};
-use substreams::pb::sf::substreams::index::v1::Keys;
+use crate::helpers::{bytes_to_utf8, parse_data, parse_mime_type, parse_value};
 use crate::pb::inscriptions::types::v1::{Transaction, Transactions};
 use substreams::errors::Error;
+use substreams::pb::sf::substreams::index::v1::Keys;
 use substreams::{log, Hex};
 use substreams_ethereum::pb::eth::v2::{Block, TransactionTraceStatus};
 
 #[substreams::handlers::map]
 fn index_transactions(transactions: Transactions) -> Result<Keys, Error> {
-    let mut keys = Keys::default();
-
-    if transactions.transactions.len() > 0 {
-        keys.keys.push("inscriptions".to_string());
-    }
-
-    Ok(keys)
+    Ok(match transactions.transactions.is_empty() {
+        true => Keys::default(),
+        false => Keys {
+            keys: vec!["inscriptions".to_string()],
+        },
+    })
 }
 
 #[substreams::handlers::map]
 pub async fn map_transactions(block: Block) -> Result<Transactions, Error> {
-    let mut transactions = vec![];
+    let transactions = block
+        .transactions()
+        .filter_map(|transaction| {
+            // Transaction must be successful
+            if transaction.status != TransactionTraceStatus::Succeeded as i32 {
+                return None;
+            }
+            // calldata
+            if transaction.input.is_empty() {
+                return None;
+            }
+            let value = match &transaction.value {
+                Some(val) => parse_value(val),
+                None => "0".to_string(),
+            };
+            let utf8 = match bytes_to_utf8(&transaction.input) {
+                Some(utf8) => utf8,
+                None => return None,
+            };
+            // Mime type (ex: "application/json")
+            let mime_type = match parse_mime_type(utf8.as_str()) {
+                Some(mime_type) => mime_type,
+                None => return None,
+            };
+            let data = match parse_data(utf8.as_str()) {
+                Some(data) => data,
+                None => return None,
+            };
 
-    for transaction in block.transactions() {
-        // Transaction must be successful
-        if transaction.status != TransactionTraceStatus::Succeeded as i32 {
-            continue;
-        }
-        // calldata
-        if transaction.input.len() == 0 {
-            continue;
-        }
-        let value = parse_value(&transaction.value); // ETH value
-        let utf8 = bytes_to_utf8(&transaction.input);
-        if utf8.is_none() {
-            continue;
-        }
-        let mime_type = parse_mime_type(utf8.as_ref().unwrap()); // Mime type (ex: "application/json")
-        if mime_type.is_none() {
-            continue;
-        }
-        let data = parse_data(utf8.as_ref().unwrap());
-        if data.is_none() {
-            continue;
-        }
-
-        transactions.push(Transaction {
-            hash: Hex(&transaction.hash).to_string(),
-            index: transaction.index,
-            from: Hex(&transaction.from).to_string(),
-            to: Hex(&transaction.to).to_string(),
-            value,
-            nonce: transaction.nonce,
-            input: Hex(&transaction.input).to_string(),
-            data: data.unwrap().to_string(),
-            mime_type: mime_type.unwrap().to_string(),
-        });
-    }
+            Some(Transaction {
+                hash: Hex(&transaction.hash).to_string(),
+                index: transaction.index,
+                from: Hex(&transaction.from).to_string(),
+                to: Hex(&transaction.to).to_string(),
+                value,
+                nonce: transaction.nonce,
+                input: Hex(&transaction.input).to_string(),
+                data: data.to_string(),
+                mime_type: mime_type.to_string(),
+            })
+        })
+        .collect::<Vec<_>>();
 
     log::debug!("Transactions: {:?}", transactions.len());
-    Ok(Transactions {
-        transactions,
-    })
+    Ok(Transactions { transactions })
 }
